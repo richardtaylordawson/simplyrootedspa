3565b8a6acb48c647a57ee71711e05c78036c2ba
Richard Dawson <richard.taylor.dawson@gmail.com>
2023-04-08 21:51:45 -0600

    wip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,68 @@
 import { Facebook, Instagram } from "@/components/Icons"
 
-const navigation = [
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/SimplyRootedSpa",
-    icon: Facebook,
-  },
-  {
-    name: "Instagram",
-    href: "https://www.instagram.com/simplyrootedspa/",
-    icon: Instagram,
-  },
-]
+const navigation = {
+  main: [
+    { name: "Menu", href: "/documents/menu.pdf", target: "" },
+    {
+      name: "Gift Cards",
+      href: "https://squareup.com/gift/9HXF0XNKEJTQ0/order",
+      target: "_blank",
+    },
+    {
+      name: "Privacy Policy",
+      href: "/privacy-policy",
+    },
+  ],
+  social: [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/SimplyRootedSpa",
+      icon: Facebook,
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/simplyrootedspa/",
+      icon: Instagram,
+    },
+  ],
+}
 
-export const Footer = () => (
-  <footer className="bg-simplyrooted-500">
-    <div className="mx-auto max-w-7xl py-12 px-6 md:flex md:items-center md:justify-between lg:px-8">
-      <div className="flex justify-center space-x-6 md:order-2">
-        {navigation.map((item) => (
-          <a
-            key={item.name}
-            href={item.href}
-            className="text-white hover:text-simplyrooted-700"
-          >
-            <span className="sr-only">{item.name}</span>
-            <item.icon className="h-6 w-6" aria-hidden="true" />
-          </a>
-        ))}
-      </div>
-      <div className="mt-8 md:order-1 md:mt-0">
-        <p className="text-center text-xl font-medium leading-5 text-white">
+export const Footer = () => {
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav
+          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+          aria-label="Footer"
+        >
+          {navigation.main.map((item) => (
+            <div key={item.name} className="pb-6">
+              <a
+                href={item.href}
+                className="text-lg font-medium leading-6 text-simplyrooted-600 hover:text-simplyrooted-900"
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <div className="mt-10 flex justify-center space-x-10">
+          {navigation.social.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-simplyrooted-400 hover:text-simplyrooted-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-10 w-10" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <p className="mt-10 text-center text-xl font-medium leading-5 text-simplyrooted-500">
           &copy; {new Date().getFullYear()} Simply Rooted Spa. All rights
           reserved.
         </p>
       </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}

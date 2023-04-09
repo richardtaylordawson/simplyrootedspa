c5acc3ee03a04248e8c5e81e6d3457e0c522752d
Richard Dawson <richard.taylor.dawson@gmail.com>
2023-04-08 22:02:53 -0600

    feat: finishing privacy policy

---
+++ src/pages/privacy-policy.jsx
@@ -0,0 +1,122 @@
+import Default from "@/templates/Default"
+
+export default function PrivacyPolicy() {
+  return (
+    <Default
+      title="Privacy Policy"
+      description="Privacy Policy for Simply Rooted Spa"
+    >
+      <div className="bg-white px-6 py-32 lg:px-8">
+        <div className="mx-auto max-w-7xl text-lg font-medium leading-7 text-simplyrooted-600">
+          <h1 class="mt-8 font-bold">Privacy Policy</h1>
+          <p class="mb-4">Last updated: 4/8/2023</p>
+          <p class="mb-4">
+            Thank you for visiting simplyrootedspa.com (the "Website"). We are
+            committed to protecting the privacy of our users. This Privacy
+            Policy explains how we collect, use, and disclose information about
+            you when you use our Website. By using the Website, you consent to
+            the collection, use, and disclosure of your information as described
+            in this Privacy Policy.
+          </p>
+          <h2 class="mt-8 font-bold">Information We Collect</h2>
+          <p class="mb-4">
+            We may collect information about you directly from you, as well as
+            automatically through your use of our Website.
+          </p>
+          <p class="mb-4">Information we collect directly from you:</p>
+          <ul>
+            <li>Contact information such as your name and email address</li>
+            <li>
+              Information you provide to us when you fill out a form or contact
+              us through our Website
+            </li>
+            <li>
+              Information you provide to us when you participate in a survey,
+              promotion, or contest
+            </li>
+          </ul>
+          <p class="mb-4">Information we collect automatically:</p>
+          <ul>
+            <li>
+              Usage information such as your IP address, browser type and
+              version, pages you view on our Website, and the time and date of
+              your visit
+            </li>
+            <li>
+              Device information such as your device type and operating system
+            </li>
+            <li>Cookies and other tracking technologies</li>
+          </ul>
+          <h2 class="mt-8 font-bold">How We Use Your Information</h2>
+          <p class="mb-4">
+            We use your information to provide and improve our Website and
+            services, and to communicate with you. We may use your information
+            for the following purposes:
+          </p>
+          <ul>
+            <li>To respond to your inquiries and provide customer support</li>
+            <li>To analyze and improve our Website and services</li>
+            <li>To personalize your experience on our Website</li>
+            <li>To send you marketing communications and promotional offers</li>
+            <li>To comply with legal obligations</li>
+          </ul>
+          <h2 class="mt-8 font-bold">How We Share Your Information</h2>
+          <p class="mb-4">
+            We may share your information with third-party service providers who
+            perform services on our behalf, such as hosting providers, email
+            service providers, and payment processors. We may also share your
+            information with our partners and affiliates for marketing purposes.
+          </p>
+          <p class="mb-4">
+            We may disclose your information as required by law, such as to
+            comply with a subpoena or other legal process, or to protect our
+            rights, property, or safety or the rights, property, or safety of
+            others. We may also share your information with third parties in
+            connection with a merger, acquisition, or sale of our business.
+          </p>
+          <h2 class="mt-8 font-bold">Your Choices</h2>
+          <p class="mb-4">
+            You may opt-out of receiving marketing communications from us by
+            following the unsubscribe instructions included in each email.
+          </p>
+          <p class="mb-4">
+            You can disable cookies in your browser settings, but please note
+            that some features of our Website may not function properly if
+            cookies are disabled.
+          </p>
+          <h2 class="mt-8 font-bold">Security</h2>
+          <p class="mb-4">
+            We take reasonable measures to protect your information from
+            unauthorized access, disclosure, or destruction. However, no
+            security measures are completely foolproof, and we cannot guarantee
+            the security of your information.
+          </p>
+          <h2 class="mt-8 font-bold">Children's Privacy</h2>
+          <p class="mb-4">
+            Our Website is not intended for children under the age of 13. We do
+            not knowingly collect personal information from children under the
+            age of 13. If we become aware that we have collected personal
+            information from a child under the age of 13, we will take steps to
+            delete the information as soon as possible.
+          </p>
+          <h2 class="mt-8 font-bold">Changes to This Privacy Policy</h2>
+          <p class="mb-4">
+            We may update this Privacy Policy from time to time. We will notify
+            you of any changes by posting the new Privacy Policy on this page.
+            We encourage you to review this Privacy Policy periodically for any
+            changes.
+          </p>
+          <h2 class="mt-8 font-bold">Contact Us</h2>
+          <p class="mb-4">
+            If you have any questions about this Privacy Policy, please contact
+            us at{" "}
+            <a className="font-bold" href="mailto:simplyrootedspa@gmail.com">
+              simplyrootedspa@gmail.com
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </Default>
+  )
+}
